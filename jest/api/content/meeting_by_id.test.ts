c958ff641b4aa221bfe5f9f64ade15f3c9b62e47
Valera-Strizhko <Valera_strizhko@mail.ru>
2021-09-07 23:58:44 +0700

    Доделал тесты и проверил их

--- jest/api/content/meeting_by_id.test.ts
+++ jest/api/content/meeting_by_id.test.ts
@@ -8,6 +8,8 @@ test('get meeting by id', async () => {
 
   const meeting = response.data;
   expect(meeting.title).toBe('London is a capital of Great Britain');
+  expect(meeting.cover.mdX2.url).toContain('/storage/a815df04-898e-4fd3-8472-8d89a15cb34d/e044f6f7-5ea1-43d3-9e62-85a51c6fe9d8/_london-mdX2.jpg');
+  expect(meeting.subject.title).toContain('Geography');
   // TODO cover.mdX2.url
   // TODO subject.title
 });
--- jest/ssr/study.test.ts
+++ jest/ssr/study.test.ts
@@ -10,16 +10,21 @@ test('Meeting page has meta', async () => {
 
   // <title>London is a capital of Great Britain, Geography, December 2nd, 2019 10:00 — 11:00, Beginner, Kaya Edwards — Study | OnClass</title>
   const title = $('title').text();
+  expect(title).toContain('London is a capital of Great Britain');
   // TODO проверить что там текст "London is a capital of Great Britain"
 
   // <meta name="description" content="Lorem ipsum dolor sit amet<...>">
   const description = $('meta[name="description"]').attr('content').toString().trim();
+  expect(description).toContain('Lorem ipsum dolor sit amet')
   // TODO Проверить что там есть текст Lorem ipsum dolor sit amet
 
-  // TODO const h1 = $('app-meeting-card h1').toString().trim();
+
+  const h1 = $('app-meeting-card h1').toString().trim();
+  expect(h1).toContain('London is a capital of Great Britain');
   // TODO проверить что там текст "London is a capital of Great Britain"
 
-  // TODO const img = $('app-meeting-card img.cover-img').attr(???).toString().trim();
+  const img = $('app-meeting-card img.cover-img').attr('src').toString().trim();
+  expect(img).toContain('/storage/a815df04-898e-4fd3-8472-8d89a15cb34d/e044f6f7-5ea1-43d3-9e62-85a51c6fe9d8/_london-md.jpg');
   // TODO проверить что там URL картинки
 });
 
